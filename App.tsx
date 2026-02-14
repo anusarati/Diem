e9ed8af566ec82aaa93a706dec0a89ed2b2feb5d
Elena Kao <elenayk1@uci.edu>
2026-02-14 14:56:05 -0800

    fix: biome format and lint (App.tsx, app.json)

--- App.tsx
+++ App.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { App as DiemApp } from './src/app';
+import { StatusBar } from "expo-status-bar";
+import { App as DiemApp } from "./src/app";
 
 export default function App() {
-  return (
-    <>
-      <StatusBar style="dark" />
-      <DiemApp />
-    </>
-  );
+	return (
+		<>
+			<StatusBar style="dark" />
+			<DiemApp />
+		</>
+	);
 }
