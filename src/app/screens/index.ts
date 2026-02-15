e292281c4692b0b4efcb518cac0c513109d4a6ad
Elena Kao <elenayk1@uci.edu>
2026-02-14 17:22:25 -0800

    feat(screens): export new screens

--- src/app/screens/index.ts
+++ src/app/screens/index.ts
@@ -1,2 +1,5 @@
 export { AnalysisScreen } from "./AnalysisScreen";
 export { HomeScreen } from "./HomeScreen";
+export { LoginScreen } from "./LoginScreen";
+export { ProfileScreen } from "./ProfileScreen";
+export { RegisterScreen } from "./RegisterScreen";
