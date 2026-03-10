1685e9727e5b7040aea32c5063a2db9d5021b73f
Elena Kao <elenayk1@uci.edu>
2026-03-09 17:14:57 -0700

    Export clearAllCalendarEvents and import types

--- src/app/data/services/index.ts
+++ src/app/data/services/index.ts
@@ -2,3 +2,9 @@ export * from "./analyticsService";
 export * from "./homeService";
 export * from "./profileService";
 export * from "./scheduleService";
+export * from "../googleCalendarApi";
+export { importGoogleCalendar } from "../googleCalendarImport";
+export type {
+	ImportGoogleCalendarOptions,
+	ImportGoogleCalendarResult,
+} from "../googleCalendarImport";
